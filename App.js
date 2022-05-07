25b0fc02cf3fd4add7648a64849c39a43ca413c9
Triveeee <riccardo,trivellato2004gmail.com>
2022-05-07 19:33:09 +0200

    aggiunta modifiche

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, Image , TextInput , View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//variabile globale
+let nome2 = "variabile globale"
+
+
+//una funzione non puo restituire piu di un valore come in python
+//nelle parentesi quadre non c'e bisogno di specificare il tipo di parametro
+//parametro e un ogetto che contiene tutti i parametri che specifichiamo all'interno dell Tag
+function Helloword (parametro) {
+    let nome = parametro.name;
+    return (
+        <Text>Ciao mondo {nome}</Text>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//funzione javaScript 
+function getName (){
+
+}
+
+//main
+function App () {
+  return (
+
+    <View>
+      <Helloword name = "Riccardo"></Helloword>
+      <Helloword name = {nome2}></Helloword>
+
+      <Image
+        source = {{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}} 
+        style = {{width : 100 , height : 100}}
+      />
+      <TextInput 
+        style = {{height : 80 , borderColor : 'gray' , borderWidth : 5 , textAlign : 'center' , margin: 10}} 
+        defaultValue = "puoi scrivere"
+      />
+    </View>
+  )
+}
+
+export default App
