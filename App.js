4a3902cd7f7653538ac84d80706df7d41c12588c
Triveeee <riccardo,trivellato2004gmail.com>
2022-05-07 21:18:15 +0200

    modifiche

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Text, Image , TextInput , View } from 'react-native';
+import { useState } from 'react/cjs/react.production.min';
 
 //variabile globale
-let nome2 = "variabile globale"
+let nome2 = "ciao"
 
 
 //una funzione non puo restituire piu di un valore come in python
 //nelle parentesi quadre non c'e bisogno di specificare il tipo di parametro
 //parametro e un ogetto che contiene tutti i parametri che specifichiamo all'interno dell Tag
-function Helloword (parametro) {
+
+function Component (parametro) {
+
     let nome = parametro.name;
+
     return (
         <Text>Ciao mondo {nome}</Text>
     )
 }
 
 //funzione javaScript 
-function getName (){
-
+function getName (name1 , name2) {
+    return (name1 + " , " + name2);
 }
 
 //main
@@ -25,19 +29,20 @@ function App () {
   return (
 
     <View>
-      <Helloword name = "Riccardo"></Helloword>
-      <Helloword name = {nome2}></Helloword>
+      <Component name = "Riccardo"></Component>
+      <Component name = {nome2}></Component>
+      <Component name = {getName("Riccardo" , "Trivellato")}></Component>
 
       <Image
         source = {{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}} 
-        style = {{width : 100 , height : 100}}
+        style = {{width : 200 , height : 200}}
       />
       <TextInput 
         style = {{height : 80 , borderColor : 'gray' , borderWidth : 5 , textAlign : 'center' , margin: 10}} 
         defaultValue = "puoi scrivere"
       />
     </View>
-  )
+  );
 }
 
 export default App
